30fd8af958fa3645b3f6fb316f1fa153308516b4
Anastasiia Ünal <velyka.nastya@icloud.com>
2024-02-03 23:58:05 +0100

    show first login data

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,17 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  email: string = '';
+  username: string = '';
   password: string = '';
-  
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 
-  login() {
-    //logik
+  async login() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "username": this.username,
+      "password": this.password
+    });
+
+    const requestOptions:RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    try {
+      let resp = await fetch("http://127.0.0.1:8000/login/", requestOptions);
+      let jsonData = await resp.json();
+      localStorage.setItem('token', jsonData.token);
+      // TODO: redirect
+    } catch(e) {
+      console.error('Error in fetch token: ', e);    
+    }
   }
 
 }
